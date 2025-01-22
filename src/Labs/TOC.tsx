5ffb5e4a70f0560f12fdcaf94da38c6af46429c6
David-the-happy-piglet <163073773+David-the-happy-piglet@users.noreply.github.com>
2025-01-22 13:07:07 -0800

    add github link

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -6,6 +6,8 @@ export default function TOC() {
             <li><Link to="/Labs/Lab1" id="wd-a1"> Lab 1 </Link></li>
             <li><Link to="/Labs/Lab2" id="wd-a2"> Lab 2 </Link></li>
             <li><Link to="/Kambaz" id="wd-a3"> Back to Kambaz </Link></li>
+            <li><a href="https://github.com/David-the-happy-piglet/kambaz-react-web-app/tree/a1"
+                id="wd-github-link" target="_blank">Github Repository</a></li>
         </ul >
     );
 }
