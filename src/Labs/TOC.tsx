c842974d3f465032a02456d4afee27256286eb2d
David-the-happy-piglet <163073773+David-the-happy-piglet@users.noreply.github.com>
2025-02-17 21:31:56 -0800

    lab3 completed

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,6 +1,9 @@
 import { Nav } from "react-bootstrap";
-/* import { Link } from "react-router-dom"; */
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
+
 export default function TOC() {
+    const { pathname } = useLocation();
     return (
         /*         <ul>
                     <li><Link to="/Labs" id="wd-a"> Labs </Link></li>
@@ -11,19 +14,27 @@ export default function TOC() {
                         id="wd-github-link" target="_blank">Github Repository</a></li>
                 </ul > */
 
-        <Nav variant="pills">
+        <Nav variant="pills" id="wd-toc">
             {/* <Nav.Item>
                 <Nav.Link href="#/Labs">Labs</Nav.Link>
             </Nav.Item> */}
             <Nav.Item>
-                <Nav.Link href="#/Labs/Lab1">Lab 1</Nav.Link>
+                {/* <Nav.Link href="#/Labs/Lab1"> //hyperlink, cause the whole page to refresh */}
+                <Nav.Link as={Link} to="/Labs/Lab1" id="wd-a1" //link provided by react-router, will not cause the whole page to refresh
+                    active={pathname.includes("Lab1")}>Lab 1</Nav.Link>
             </Nav.Item>
+
+            <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab2" id="wd-a2"
+                active={pathname.includes("Lab2")}> Lab 2 </Nav.Link> </Nav.Item>
+
+            <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab3" id="wd-a3"
+                active={pathname.includes("Lab3")}> Lab 3 </Nav.Link> </Nav.Item>
+
+
             <Nav.Item>
-                <Nav.Link href="#/Labs/Lab2">Lab 2</Nav.Link>
+                <Nav.Link as={Link} to="/Kambaz" id="wd-a3">Kambaz</Nav.Link>
             </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
-            </Nav.Item>
+
             <Nav.Item>
                 <Nav.Link href="https://github.com/David-the-happy-piglet/kambaz-react-web-app/tree/a2">Github Repository</Nav.Link>
             </Nav.Item>
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Lab1 from "./Lab1";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
+
 export default function Labs() {
     return (
         <div>
@@ -14,6 +16,7 @@ export default function Labs() {
             <Routes>
                 <Route path="Lab1/*" element={<Lab1 />} />
                 <Route path="Lab2/*" element={<Lab2 />} />
+                <Route path="Lab3/*" element={<Lab3 />} />
             </Routes>
         </div>
 
